4b796512a58ad772aed76f514d8b536aef2a2e4b
Bart Liszka <bartlomiej.liszka@gamil.com>
2025-11-26 11:03:19 +0100

    Updated for trouble symbols bar size in Neovim config

---
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      symbols = {
+        auto_open = false,
+        auto_close = false,
+        win = {
+          position = "right",
+          size = 0.2,
+        },
+      },
+    },
+  },
+}
